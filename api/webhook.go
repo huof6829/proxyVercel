0b0d8f4365effaa773b3e094300bda9e9d7f3a25
huof6829 <68298506@qq.com>
2024-08-08 17:44:32 +0000

    new

---
+++ api/webhook.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func HandleWebhook(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("proxy HandleWebhook")
+
+	targetURL := "http://ec2-52-77-241-219.ap-southeast-1.compute.amazonaws.com:8443/webhook"
+	// targetURL := "http://localhost:8888"
+
+	url, err := url.Parse(targetURL)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error parsing target URL: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	// 修改请求主机头，确保目标服务器能正确处理
+	r.Host = url.Host
+
+	// 执行反向代理
+	proxy.ServeHTTP(w, r)
+
+}
--- main.go
+++ main.go
@@ -3,34 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
+	handler "proxyvercel/api"
 )
 
-func HandleWebhook(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("proxy HandleWebhook")
-
-	targetURL := "http://ec2-52-77-241-219.ap-southeast-1.compute.amazonaws.com:8443/webhook"
-	// targetURL := "http://localhost:8888"
-
-	url, err := url.Parse(targetURL)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error parsing target URL: %s", err), http.StatusInternalServerError)
-		return
-	}
-
-	proxy := httputil.NewSingleHostReverseProxy(url)
-
-	// 修改请求主机头，确保目标服务器能正确处理
-	r.Host = url.Host
-
-	// 执行反向代理
-	proxy.ServeHTTP(w, r)
-
-}
-
 func main() {
-	http.HandleFunc("/webhook", HandleWebhook)
+	http.HandleFunc("/webhook", handler.HandleWebhook)
 
 	port := ":8443"
 	fmt.Printf("Starting server on port %s\n", port)
